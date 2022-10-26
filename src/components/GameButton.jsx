694461c6200b8dedf8f57a5be806af7079a3d504
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-10-26 17:49:33 +0200

    update css

--- src/components/GameButton.jsx
+++ src/components/GameButton.jsx
@@ -1,15 +1,23 @@
 import './GameButton.css';
 
-const GameButton = ({ TL, TR, BL, BR, color }) => {
+const GameButton = ({ TL, TR, BL, BR, color, top, left }) => {
 
-const styles = {
-    backgroundColor: `${color}`,
-    borderRadius: `${TL? TL : 0}px ${TR? TR : 0}px ${BR? BR : 0}px ${BL? BL : 0}px`
-} 
+  const buttonStyles = {
+    backgroundColor: color,
+    borderRadius: `${TL ? TL : 0}px ${TR ? TR : 0}px ${BR ? BR : 0}px ${BL ? BL : 0}px`
+  }
+
+  const absStyles = {
+    backgroundColor: 'inherit',
+    top: top,
+    left: left,
+    borderRadius: 'inherit',
+  }
 
   return (
-    <div type='button' className='button' style={styles}>
-    </div>
+      <div type='button' className='button' style={buttonStyles} >
+        <div className='active' style={absStyles} />
+      </div>
   );
 }
 
--- src/components/Simon.jsx
+++ src/components/Simon.jsx
@@ -5,10 +5,10 @@ import './Simon.css';
 const Simon = () => {
   return (
     <div className='board'>
-      <GameButton color='#ff0000' TL={600} />
-      <GameButton color='#297fb8' TR={600} />
-      <GameButton color='#27ae61' BL={600} />
-      <GameButton color='#f1c40f' BR={600} />
+      <GameButton color='#ff0000' TL='600' top='-5%' left='-5%' />
+      <GameButton color='#297fb8' TR='600' top='-5%' left='5%' />
+      <GameButton color='#27ae61' BL='600' top='5%' left='-5%' />
+      <GameButton color='#f1c40f' BR='600' top='5%' left='5%' />
       <Control />
     </div>
   );
