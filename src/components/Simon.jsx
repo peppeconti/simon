2eadb0e85270731eae0a901d2ce6779019fcde5e
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-10-26 20:14:04 +0200

    added buttons array

--- src/components/Simon.jsx
+++ src/components/Simon.jsx
@@ -3,12 +3,37 @@ import Control from './Control';
 import './Simon.css';
 
 const Simon = () => {
+
+  const GameButtons = [
+    {
+      color: '#ff0000',
+      TL: '600',
+      top: '-5%',
+      left: '-5%'
+    },
+    {
+      color: '#297fb8',
+      TR: '600',
+      top: '-5%',
+      left: '5%'
+    },
+    {
+      color: '#27ae61',
+      BL: '600',
+      top: '5%',
+      left: '-5%'
+    },
+    {
+      color: '#f1c40f',
+      BR: '600',
+      top: '5%',
+      left: '5%'
+    }
+  ]
+
   return (
     <div className='board'>
-      <GameButton color='#ff0000' TL='600' top='-5%' left='-5%' />
-      <GameButton color='#297fb8' TR='600' top='-5%' left='5%' />
-      <GameButton color='#27ae61' BL='600' top='5%' left='-5%' />
-      <GameButton color='#f1c40f' BR='600' top='5%' left='5%' />
+      {GameButtons.map((obj, i) => <GameButton key={i} className='button' color={obj.color} top={obj.top} left={obj.left} />)}
       <Control />
     </div>
   );
