6504d838c868a3815ea50b0a8d09a6b8a9bda743
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-10-26 19:22:13 +0200

    update css

--- src/components/GameButton.jsx
+++ src/components/GameButton.jsx
@@ -1,24 +1,29 @@
 import './GameButton.css';
 
-const GameButton = ({ TL, TR, BL, BR, color, top, left }) => {
+const GameButton = ({ TL, TR, BL, BR, color }) => {
 
   const buttonStyles = {
     backgroundColor: color,
-    borderRadius: `${TL ? TL : 0}px ${TR ? TR : 0}px ${BR ? BR : 0}px ${BL ? BL : 0}px`
+    borderRadius: `${TL ? TL : 0}px ${TR ? TR : 0}px ${BR ? BR : 0}px ${BL ? BL : 0}px`,
+    borderLeft: `${BR || TR ? '.5rem solid #fff' : 'none'}`,
+    borderTop: `${BL || BR ? '.5rem solid #fff' : 'none'}`,
+    borderRight: `${TL || BL ? '.5rem solid #fff' : 'none'}`,
+    borderBottom: `${TL || TR ? '.5rem solid #fff' : 'none'}`,
   }
 
-  const absStyles = {
+  /*const absStyles = {
     backgroundColor: 'inherit',
     top: top,
     left: left,
     borderRadius: 'inherit',
-  }
+  }*/
 
   return (
-      <div type='button' className='button' style={buttonStyles} >
-        <div className='active' style={absStyles} />
-      </div>
+    <div type='button' className='button' style={buttonStyles} />
   );
 }
 
 export default GameButton;
+
+
+/*{TL && <div className='active' style={absStyles} />}*/
