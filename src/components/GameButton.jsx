8fc1313f7a4e703b7e4ae57c53a41bbd97f78992
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-10-29 21:49:07 +0200

    update

--- src/components/GameButton.jsx
+++ src/components/GameButton.jsx
@@ -8,10 +8,10 @@ const GameButton = forwardRef(({ border, color, id, player, checkPlayer }, ref) 
   const buttonStyles = {
     backgroundColor: color,
     borderRadius: `${border === 'TL' ? 600 : 0}px ${border === 'TR' ? 600 : 0}px ${border === 'BR' ? 600 : 0}px ${border === 'BL' ? 600 : 0}px`,
-    borderLeft: `${border === 'BR' || border === 'TR' ? '.5rem solid #fff' : 'none'}`,
-    borderTop: `${border === 'BR' || border === 'BL' ? '.5rem solid #fff' : 'none'}`,
-    borderRight: `${border === 'TL' || border === 'BL' ? '.5rem solid #fff' : 'none'}`,
-    borderBottom: `${border === 'TL' || border === 'TR' ? '.5rem solid #fff' : 'none'}`,
+    borderLeft: `${border === 'BR' || border === 'TR' ? '.5rem solid #15263F' : 'none'}`,
+    borderTop: `${border === 'BR' || border === 'BL' ? '.5rem solid #15263F' : 'none'}`,
+    borderRight: `${border === 'TL' || border === 'BL' ? '.5rem solid #15263F' : 'none'}`,
+    borderBottom: `${border === 'TL' || border === 'TR' ? '.5rem solid #15263F' : 'none'}`,
   };
 
   const audio = useMemo(() => new Audio(audio_files[id]), [id])
