e2d90ef3310c23a6455f56fca6d552c06d179372
peppeconti <46853326+peppeconti@users.noreply.github.com>
2022-10-31 20:24:44 +0100

    added media queries

--- src/components/GameButton.jsx
+++ src/components/GameButton.jsx
@@ -1,16 +1,33 @@
 import { forwardRef, useImperativeHandle, useState } from 'react';
-import './GameButton.css';
 import { motion } from "framer-motion";
+import { useMediaQuery } from '../hook/useMediaQuery';
+import './GameButton.css';
 
 const GameButton = forwardRef(({ border, color, id, audio, player, checkSequence }, ref) => {
+
+  const medium = useMediaQuery('(max-width: 414px)');
+  const small = useMediaQuery('(max-width: 375px)');
+  const smaller = useMediaQuery('(max-width: 280px)');
+
+  let boardSize;
+
+  if (smaller) {
+    boardSize = '.15rem';
+  } else if (small) {
+    boardSize ='.25rem';
+  } else if (medium) {
+    boardSize ='.3rem'; 
+  } else {
+    boardSize ='.5rem';
+  }
 
   const buttonStyles = {
     backgroundColor: color,
     borderRadius: `${border === 'TL' ? 600 : 0}px ${border === 'TR' ? 600 : 0}px ${border === 'BR' ? 600 : 0}px ${border === 'BL' ? 600 : 0}px`,
-    borderLeft: `${border === 'BR' || border === 'TR' ? '.5rem solid #15263F' : 'none'}`,
-    borderTop: `${border === 'BR' || border === 'BL' ? '.5rem solid #15263F' : 'none'}`,
-    borderRight: `${border === 'TL' || border === 'BL' ? '.5rem solid #15263F' : 'none'}`,
-    borderBottom: `${border === 'TL' || border === 'TR' ? '.5rem solid #15263F' : 'none'}`,
+    borderLeft: `${border === 'BR' || border === 'TR' ? `${boardSize} solid #15263F` : 'none'}`,
+    borderTop: `${border === 'BR' || border === 'BL' ? `${boardSize} solid #15263F` : 'none'}`,
+    borderRight: `${border === 'TL' || border === 'BL' ? `${boardSize} solid #15263F` : 'none'}`,
+    borderBottom: `${border === 'TL' || border === 'TR' ? `${boardSize} solid #15263F` : 'none'}`,
   };
 
   const [animation, setAnimation] = useState(false);
